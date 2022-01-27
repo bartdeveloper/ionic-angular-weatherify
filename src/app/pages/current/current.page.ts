b8aeee437342f884f447c7640f8ccd29a53ab9b5
bartdeveloper <bartek@appselio.pl>
2022-01-27 23:42:28 +0100

    Implemented reactive forms in current weather component

--- src/app/pages/current/current.page.ts
+++ src/app/pages/current/current.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CurrentWeather } from 'src/app/models/CurrentWeather';
 import { APIService } from 'src/app/services/api.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-current',
@@ -12,10 +13,19 @@ export class CurrentPage implements OnInit {
   data: CurrentWeather;
   error;
   
+  weatherForm = new FormGroup({
+    cityName: new FormControl('Gdansk', [Validators.required, Validators.minLength(3)])
+  })
+
   constructor(private api: APIService) { }
 
   ngOnInit() {
     this.api.getCurrentWeather('Gdansk').subscribe((data) => this.data = data, (err) => this.error = err.error.error.message);
+  }
+
+  onSubmit() {
+    console.log(this.weatherForm.value);
+    this.api.getCurrentWeather(this.weatherForm.value.cityName).subscribe((data) => { this.data = data; this.error = null; }, (err) => this.error = err.error.error.message);
   }
 
 }
